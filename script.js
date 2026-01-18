b0b7a7e129a7a881132c4f0fb6d109c53f7e9f07
Geof-0 <geckoeater61@gmail.com>
2026-01-17 20:50:57 -0600

    4
    1, 2, 3, 4, 1, 2, 3, 4

---
+++ script.js
@@ -0,0 +1,75 @@
+console.log('js paired')
+
+
+const socket = io("http://127.0.0.1:6767")
+
+// chat input
+
+const textInput = document.getElementById("text-input")
+
+let currentText = textInput.textContent
+
+textInput.addEventListener('input', () => {
+    currentText = textInput.textContent
+    console.log(currentText)
+})
+
+textInput.addEventListener('keydown', (e) => {
+    if (e.key === "Enter"){
+        textInput.blur()
+        console.log('new message: ' + currentText)
+        
+        if (textInput.textContent == 'type here'){
+            return
+        }
+
+        socket.emit('new_chat_toserver', {'msg': currentText})
+        textInput.textContent = 'type here'
+        currentText = textInput.textContent
+    }
+})
+
+textInput.addEventListener('focus', () => {
+    console.log('selected textInput')
+    if (textInput.textContent == 'type here'){
+        textInput.textContent = ''
+    }
+});
+
+textInput.addEventListener('blur', () => {
+    console.log('unselected textInput')
+    if (textInput.textContent == ''){
+        textInput.textContent = 'type here'
+    }
+});
+
+
+
+
+// chat menu & new texts
+
+
+const chatMenu = document.getElementById("chat-menu")
+
+class textBox{
+    constructor(text){
+        this.element = document.createElement('div')
+        this.element.classList.add('chat-bubble') 
+        this.element.textContent = text
+
+        chatMenu.appendChild(this.element)
+    }
+}
+
+
+
+
+
+
+// backend linking
+
+
+socket.on('new_chat_toclient', data => {
+    console.log(data['msg'])
+    newTextBox = new textBox(data['msg'])
+})
